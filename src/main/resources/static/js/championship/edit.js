468eaabba67cc769a1330d1dbb1be29b6e63ed3e
OKB84 <44141422+OKB84@users.noreply.github.com>
2020-12-31 10:36:53 +0900

    タイムゾーン再設定

--- src/main/resources/static/js/championship/edit.js
+++ src/main/resources/static/js/championship/edit.js
@@ -144,7 +144,7 @@ var app = new Vue({
 				})
 				.then(data => {
 					// 基本情報をフォームに受け渡し
-					const convertedDate = new Date(data.date).toLocaleString({ timeZone: 'Asia/Tokyo' });
+					const convertedDate = new Date(((new Date(data.date).getTimezoneOffset() + (9 * 60)) * 60 * 1000));
 					const year = convertedDate.getFullYear();
 					const month = ("0"+(convertedDate.getMonth() + 1)).slice(-2);	// 0埋めを行う
 					const date = ("0"+(convertedDate.getDate())).slice(-2);			// 0埋めを行う
